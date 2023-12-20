081734d0532bdc2759801d80fc0cc021eab6f395
Kate <kate.marshalkina@onlyfy.com>
2023-12-20 18:39:33 +0100

    add advent-of-frontend-architecture-2023 article (#99)

--- src/components/bio.js
+++ src/components/bio.js
@@ -34,6 +34,7 @@ function Bio() {
         I love solving problems regardless of the type of work: from basic client support to advanced devops tasks. I do it better when I understand how things work, but sometimes it just feels like magic.
         </p>
         <p>
+          <a href="https://hachyderm.io/@kalabro">Mastodon</a> ~{" "}
           <a href="https://twitter.com/kalabro">Twitter</a> ~{" "}
           <a href="https://www.linkedin.com/in/kalabro/">LinkedIn</a> ~{" "}
           <a href="https://topmate.io/kalabro">Mentorship</a>
